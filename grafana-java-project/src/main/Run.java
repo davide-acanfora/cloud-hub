e454ec8165673102fb295b115c0b793812805e9a
davide-acanfora <davide.acanfora@gmail.com>
2019-09-13 10:19:43 +0200

    Testing .gitignore 2

--- grafana-java-project/src/main/Run.java
+++ grafana-java-project/src/main/Run.java
@@ -8,7 +8,7 @@ public class Run {
 	public static void main(String[] args) throws IOException {
 		Grafana grafana = new Grafana(3000, false);
 		grafana.enableLocalMonitoring(8080, 12000);
-		grafana.enableCloudWatchMonitoring("test1", "test2", "us-east-1");
+		grafana.enableCloudWatchMonitoring("test3", "test3", "us-east-1");
 		grafana.start();
 		//grafana.stop();
 	}
