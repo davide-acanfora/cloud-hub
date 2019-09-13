f358a957ec67dedcabd82a000132b0f1f0e3c3a4
mario-donnarumma <mario.donnarumma.97@gmail.com>
2019-09-13 11:26:16 +0200

    Aggiunta dashboard AWS

---
+++ grafana-java-project/src/grafana/dashboard/AWSDashboard.java
@@ -0,0 +1,62 @@
+package grafana.dashboard;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import grafana.Configurable;
+import grafana.datasource.CloudWatchDataSource;
+
+public class AWSDashboard implements Configurable{
+	private File file;
+	private CloudWatchDataSource cloudWatchDataSource;
+	
+	public AWSDashboard(CloudWatchDataSource cloudWatchDataSource) {
+		this.cloudWatchDataSource = cloudWatchDataSource;
+	}
+
+	@Override
+	public void createConfig(String grafanaPath) {
+		try {
+			String dashboard = new String(Files.readAllBytes(Paths.get(grafanaPath+"/conf/provisioning/dashboards/awsdashboard.template")));
+			
+			dashboard = dashboard.replaceAll("\\$cloudwatch-datasource-name", cloudWatchDataSource.getName());
+			dashboard = dashboard.replaceAll("\\$cloudwatch-default-region", cloudWatchDataSource.getDefaultRegion());
+			
+			file = new File(grafanaPath+"/conf/provisioning/dashboards/awsdashboard.json");
+			Writer writer = null;
+			
+			try{
+				writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+				writer.write(dashboard);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void deleteConfig() {
+		file.delete();
+		cloudWatchDataSource.deleteConfig();
+	}
+
+	
+}
